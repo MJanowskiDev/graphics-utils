45531824cca1671b6c91fc989afe78d400e8cedf
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-20 21:30:49 +0200

    feat: implemented asset entity, storing links into db

--- src/image-processing/resize/resize.service.ts
+++ src/image-processing/resize/resize.service.ts
@@ -1,19 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import sharp from 'sharp';
 import { ImagesBucketService } from '../images-bucket/images-bucket.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Asset } from '../asset.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ResizeService {
-  constructor(private imagesBucketService: ImagesBucketService) {}
+  constructor(
+    private imagesBucketService: ImagesBucketService,
+    @InjectRepository(Asset)
+    private readonly assetRepository: Repository<Asset>,
+  ) {}
   async resize(
     inputBuffer: Buffer,
     width: number,
     fileName: string,
   ): Promise<Buffer> {
     try {
-      await this.imagesBucketService.storePrivate(inputBuffer, fileName);
+      const asset = new Asset();
+      const sourceLink = await this.imagesBucketService.storePrivate(
+        inputBuffer,
+        fileName,
+      );
+      asset.sourceLink = sourceLink.Location;
+
+      const { id } = await this.assetRepository.save(asset);
+
       const buffer = await sharp(inputBuffer).resize({ width }).toBuffer();
-      await this.imagesBucketService.storePublic(buffer, fileName);
+      const publicLink = await this.imagesBucketService.storePublic(
+        buffer,
+        fileName,
+      );
+
+      await this.assetRepository.update(id, {
+        outputLink: publicLink.Location,
+      });
+
       return buffer;
     } catch (error) {
       throw error;
