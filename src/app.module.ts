be29f71d96fcc3ef919b1a1b0362ac262605677d
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-20 21:30:16 +0200

    feat: moved typeorm to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageProcessingModule } from './image-processing/image-processing.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import AwsConfig from './config/aws';
 import S3Config from './config/s3-buckets';
 import DatabaseConfig from './config/database';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+import { Asset } from './image-processing/asset.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<DataSourceOptions> => {
+        const { dbHost, dbPort, dbUsername, dbPassword, dbType, dbName } =
+          configService.get('database');
+        return {
+          type: dbType,
+          host: dbHost,
+          port: dbPort,
+          username: dbUsername,
+          password: dbPassword,
+          database: dbName,
+          entities: [Asset],
+          synchronize: true,
+        };
+      },
+    }),
     ImageProcessingModule,
     ThrottlerModule.forRoot({
       ttl: 60,
--- src/image-processing/image-processing.module.ts
+++ src/image-processing/image-processing.module.ts
@@ -1,36 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ResizeController } from './resize/resize.controller';
 import { ResizeService } from './resize/resize.service';
 import { ImagesBucketService } from './images-bucket/images-bucket.service';
 import { ConvertController } from './convert/convert.controller';
 import { ConvertService } from './convert/convert.service';
+import { Asset } from './asset.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSourceOptions } from 'typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (
-        configService: ConfigService,
-      ): Promise<DataSourceOptions> => {
-        const { dbHost, dbPort, dbUsername, dbPassword, dbType, dbName } =
-          configService.get('database');
-        return {
-          type: dbType,
-          host: dbHost,
-          port: dbPort,
-          username: dbUsername,
-          password: dbPassword,
-          database: dbName,
-          entities: [],
-          synchronize: true,
-        };
-      },
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Asset])],
   controllers: [ResizeController, ConvertController],
   providers: [ResizeService, ImagesBucketService, ConvertService],
 })
