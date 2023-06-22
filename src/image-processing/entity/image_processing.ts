04d9f26e46d2697710a527c0359ae6870af787dd
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-22 21:16:53 +0200

    feat: implemented image_processing entity

---
+++ src/image-processing/entity/image_processing.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { OperationType } from '../types';
+import { Asset } from './asset.entity';
+import { OperationData } from './operation_data.entity';
+
+@Entity()
+export class ImageProcessing {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'enum', enum: OperationType })
+  type: OperationType;
+
+  @OneToOne(() => Asset)
+  @JoinColumn()
+  assetsId: number;
+
+  @OneToOne(() => OperationData)
+  @JoinColumn()
+  operationDataId: number;
+
+  @Column({ nullable: true, type: 'json' })
+  userParams: string;
+
+  @Column({ nullable: true, type: 'json' })
+  inputDescription: string;
+
+  @Column({ nullable: true, type: 'json' })
+  outputDescription: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
