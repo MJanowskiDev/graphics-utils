21b716be57936d10a2869bdf0383e0faeb92d1c0
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-25 18:58:29 +0200

    feat: omitted hashedPasword from register request response

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { User } from './entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostgresError } from 'pg-error-enum';
+import { omit } from 'lodash';
 
 @Injectable()
 export class UsersService {
@@ -15,13 +16,16 @@ export class UsersService {
     return this.userRepositry.findOneBy({ email });
   }
 
-  async create(email: string, hashedPassword: string): Promise<User | null> {
+  async create(
+    email: string,
+    hashedPassword: string,
+  ): Promise<Omit<User, 'hashedPassword'> | null> {
     const user = new User();
     user.email = email;
     user.hashedPassword = hashedPassword;
     try {
       const createdUser = await this.userRepositry.save(user);
-      return createdUser;
+      return omit(createdUser, 'hashedPassword');
     } catch (error) {
       if (error.code === PostgresError.UNIQUE_VIOLATION) {
         throw new ConflictException('This email already exists.');
