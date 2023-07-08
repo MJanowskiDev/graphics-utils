101058ea16364dd4d05100ed298e70907adefa37
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-07-08 10:24:13 +0200

    feat: added error throw when no file passed

--- src/core/decorator/AttachInputFileHeader.decorator.ts
+++ src/core/decorator/AttachInputFileHeader.decorator.ts
@@ -7,12 +7,14 @@ import {
 
 export const AttachInputFileHeader = createParamDecorator(
   (_data: never, ctx: ExecutionContext) => {
-    const { response } = ctx.switchToHttp().getResponse();
+    const response = ctx.switchToHttp().getResponse();
+    const request = ctx.switchToHttp().getRequest();
 
-    if (!response?.file) {
+    if (!request?.file) {
       throw new HttpException('No input file passed', HttpStatus.BAD_REQUEST);
     }
-    const { mimetype, originalname } = response;
+
+    const { mimetype, originalname } = request.file;
 
     response.set({
       'Content-Type': mimetype,
