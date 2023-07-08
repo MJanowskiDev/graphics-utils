948180776523934b2c0df762b1b410671063a284
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-07-08 10:16:03 +0200

    feat: error throwing in AttachInputFileHeader when no file passed.

--- src/core/decorator/AttachInputFileHeader.decorator.ts
+++ src/core/decorator/AttachInputFileHeader.decorator.ts
@@ -1,9 +1,18 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 
 export const AttachInputFileHeader = createParamDecorator(
   (_data: never, ctx: ExecutionContext) => {
-    const response = ctx.switchToHttp().getResponse();
-    const { mimetype, originalname } = ctx.switchToHttp().getRequest().file;
+    const { response } = ctx.switchToHttp().getResponse();
+
+    if (!response?.file) {
+      throw new HttpException('No input file passed', HttpStatus.BAD_REQUEST);
+    }
+    const { mimetype, originalname } = response;
 
     response.set({
       'Content-Type': mimetype,
