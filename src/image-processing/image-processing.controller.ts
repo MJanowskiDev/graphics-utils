c7cc19bf62dddbd421ebe346be3e3a5faf0b51a5
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-07-08 10:36:03 +0200

    feat: merged convert and resize controllers into one image-processing controller

--- src/image-processing/image-processing.controller.ts
+++ src/image-processing/image-processing.controller.ts
@@ -10,14 +10,35 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { ConvertDto } from './dto/format.dto';
+import { AttachInputFileHeader } from '../core/decorator/AttachInputFileHeader.decorator';
+import { ResizeImageDto } from './resize/dto/resize.dto';
+import { UploadedFileValidation } from './validation/uploaded-file.validation';
 import { Response } from 'express';
-import { ConvertService } from './convert.service';
-import { UploadedFileValidation } from '../validation/uploaded-file.validation';
+import { ResizeService } from './resize/resize.service';
+import { ConvertDto } from './convert/dto/format.dto';
+import { ConvertService } from './convert/convert.service';
 
-@Controller('convert')
-export class ConvertController {
-  constructor(private convertService: ConvertService) {}
+@Controller('image')
+export class ImageProcessingController {
+  constructor(
+    private resizeService: ResizeService,
+    private convertService: ConvertService,
+  ) {}
+
+  @Post('resize')
+  @UseInterceptors(FileInterceptor('image'))
+  async resizeImage(
+    @UploadedFile(UploadedFileValidation)
+    file: Express.Multer.File,
+    @Query(new ValidationPipe({ transform: true }))
+    { width }: ResizeImageDto,
+    @AttachInputFileHeader()
+    res: Response,
+  ) {
+    res.send(
+      await this.resizeService.resize(file.buffer, width, file.originalname),
+    );
+  }
 
   @Post()
   @UseInterceptors(FileInterceptor('image'))
--- src/image-processing/image-processing.module.ts
+++ src/image-processing/image-processing.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ResizeController } from './resize/resize.controller';
 import { ResizeService } from './resize/resize.service';
 import { ImagesBucketService } from './images-bucket/images-bucket.service';
-import { ConvertController } from './convert/convert.controller';
 import { ConvertService } from './convert/convert.service';
 import { Asset, OperationData, ImageProcessing } from './entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssetRepository, OperationDataRepository } from './repository';
+import { ImageProcessingController } from './image-processing.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Asset, OperationData, ImageProcessing])],
-  controllers: [ResizeController, ConvertController],
+  controllers: [ImageProcessingController],
   providers: [
     ResizeService,
     ImagesBucketService,
