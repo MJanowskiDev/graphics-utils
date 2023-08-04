d8f0825c31986090d322590781d7d6a4e33d20dd
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-08-04 22:00:53 +0200

    feat: implemented welcome frontend app page

---
+++ frontend/graphics-utils/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Graphics Utils',
+  description:
+    'Offering a variety of operations, from basic adjustments to advanced, AI-driven transformations.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/graphics-utils/src/app/page.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import logo from '../../public/images/graphics_utils_logo.svg';
+import author from '../../public/images/mjanowskidev.png';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-stretch justify-center min-h-screen bg-gradient-to-b from-10% from-black via-slate-900 to-purple-950 font-sans">
+      <section className="flex flex-row flex-wrap sm:gap-12 gap-10 justify-center items-center ">
+        <div className="w-[320px]">
+          <Image src={logo} width={320} height={47} alt="Graphics Utils Logo" />
+        </div>
+        <div className="sm:text-left text-center">
+          <h1 className="text-2xl max-w-lg font-bold sm:mb-6 mb-10 text-white tracking-tight">
+            Image processing tools
+          </h1>
+          <p className="text-gray-300 tracking-wide">
+            Offering a variety of operations, from basic adjustments to
+            advanced,{' '}
+            <span className="font-bold text-purple-500">AI-driven</span>{' '}
+            transformations.
+          </p>
+          <p className=" text-gray-300 sm:mt-1 mt-4">
+            Stay tuned for our launch!
+          </p>
+        </div>
+      </section>
+      <div className="sticky top-[100vh] flex gap-2 p-4 justify-between text-[#6c757dff] tracking-wide text-sm">
+        <div className="flex items-center gap-2">
+          <p>Created by</p>
+          <div className="mt-0.35">
+            <Image src={author} width={95} height={6} alt="MJanowskiDevLogo" />
+          </div>
+        </div>
+
+        <Link
+          href="https://mjanowski.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Homepage
+        </Link>
+      </div>
+    </main>
+  );
+}
