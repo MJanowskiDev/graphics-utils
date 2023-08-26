16ef7f28debb203c46f9c94fe5a8afc85e5d661e
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-08-26 16:28:46 +0200

    feat:(fe) implemented metatags

--- frontend/graphics-utils/src/app/layout.tsx
+++ frontend/graphics-utils/src/app/layout.tsx
@@ -1,21 +1,17 @@
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { Footer, Navbar } from '@/features/ui';
 import Providers from '@/utils/provider';
+import { DefaultMetatags } from '@/features/metatags';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Graphics Utils - AI-Driven Image Processing Tools',
-  description: 'Offering a variety of operations, from basic adjustments to advanced, AI-driven transformations.',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <DefaultMetatags />
         <Providers>
           <div className="flex flex-col min-h-screen bg-gradient-to-b from-10% from-black via-slate-900 to-purple-950 font-sans">
             <Navbar />
